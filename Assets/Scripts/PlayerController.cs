f50d47f79ab8f2d675bc9e0f3f488774b72935dc
Maya Saietz <ms@tec.dk>
2022-02-15 10:33:43 +0100

    horizontal movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        anim.SetFloat("moveX", horizontal);
+        // Debug.Log(horizontal);
+
+        Vector3 position = transform.position;
+        position.x += horizontal * moveSpeed * Time.deltaTime;
+        transform.position = position;
     }
 }
